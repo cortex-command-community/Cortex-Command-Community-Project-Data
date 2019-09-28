134d0e26680ecc3e066f633fa66da03f4fb34e20
MaximDude <maximdude@gmail.com>
2019-09-28 08:57:38 +0300

    Move smoke trail code into Base Effects Pyro Move smoke trail script into Base Scripts Shared

--- Base.rte/Scripts/Shared/SmokeTrail.lua
+++ Base.rte/Scripts/Shared/SmokeTrail.lua
@@ -4,7 +4,7 @@ function Update(self)
 	local Offset = self.Vel*(20*TimerMan.DeltaTimeSecs)	-- the effect will be created the next frame so move it one frame backwards towards the barrel
 	
 	for i = 1, math.floor(self.Vel.Magnitude*0.045) do
-		Effect = CreateMOSParticle("Tiny Smoke Trail " .. math.random(3), "Coalition.rte")
+		Effect = CreateMOSParticle("Tiny Smoke Trail " .. math.random(3), "Base.rte")
 		if Effect then
 			Effect.Pos = self.Pos - Offset * i/8 + Vector(RangeRand(-2,2),RangeRand(-2,2))
 			Effect.Vel = (self.Vel + Vector(RangeRand(-10,30),RangeRand(-10,10))) / 20
