23c62e207d3f9639995aaf1ce92e534302aa43f7
MaximDude <maximdude@gmail.com>
2019-10-05 02:05:11 +0300

    Correcting filepaths Code cleanup Renaming files

--- Ronin.rte/Devices/Weapons/Rifles/M1Garand/Garand.lua
+++ Ronin.rte/Devices/Weapons/Rifles/M1Garand/Garand.lua
@@ -1,34 +1,25 @@
 function Create(self)
-
-	self.dinSound = false;
+	self.pingSound = false;
 	self.lastAmmo = 0;
-
 end
 
 function Update(self)
-
 	if self.Magazine ~= nil then
 		self.lastMag = self.Magazine;
 		self.lastAmmo = self.Magazine.RoundCount;
-		self.dingSound = false;
+		self.pingSound = false;
 		if self.Magazine.RoundCount <= 0 then
 			self:Reload();
 		end
 	else
-		if self.dingSound == false and self.lastAmmo <= 0 then
-			local soundfx = CreateAEmitter("Ronin M1 Garand Sound Ding");
-			soundfx.Pos = self.Pos;
-			MovableMan:AddParticle(soundfx);
+		if self.pingSound == false and self.lastAmmo <= 0 then
+			local soundEffect = CreateAEmitter("Ronin M1 Garand Sound Ping","Ronin.rte");
+			soundEffect.Pos = self.Pos;
+			MovableMan:AddParticle(soundEffect);
 			if MovableMan:IsParticle(self.lastMag) then
-				if self.HFlipped == false then
-					self.negativeNum = 1;
-				else
-					self.negativeNum = -1;
-				end
-				self.lastMag.Vel = self.lastMag.Vel + Vector(-4*self.negativeNum,-7):RadRotate(self.RotAngle);
+				self.lastMag.Vel = self.lastMag.Vel + Vector(-4 * self.FlipFactor, -7):RadRotate(self.RotAngle);
 			end
 		end
-		self.dingSound = true;
+		self.pingSound = true;
 	end
-
 end
