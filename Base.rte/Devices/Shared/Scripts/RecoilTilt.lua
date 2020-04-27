e3d14574245764553df51d136d99ff61c0441e98
4zk <the4zk@hotmail.com>
2020-04-27 21:41:08 +0300

    Added some clamping and randomization to RecoilTilt.lua

--- Base.rte/Devices/Shared/Scripts/RecoilTilt.lua
+++ Base.rte/Devices/Shared/Scripts/RecoilTilt.lua
@@ -1,16 +1,16 @@
 function Create(self)
 	self.setAngle = 0;
-	self.tilt = 0.2;
+	self.tilt = 0.5 / math.sqrt(self.Radius);
 end
 function Update(self)
 	if self.setAngle > 0 then
-		self.setAngle = self.setAngle - 0.0001 * self.RateOfFire;
+		self.setAngle = self.setAngle - (0.001 * (10 + math.sqrt(self.RateOfFire) * self.setAngle));
 		if self.setAngle < 0 then
 			self.setAngle = 0;
 		end
 	end
 	if self.FiredFrame then
-		self.setAngle = self.setAngle + self.tilt;
+		self.setAngle = self.setAngle + self.tilt * math.random();
 	end
 	self.RotAngle = self.RotAngle + self.setAngle * self.FlipFactor;
 	local jointOffset = Vector(self.JointOffset.X * self.FlipFactor, self.JointOffset.Y):RadRotate(self.RotAngle);
