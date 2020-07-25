3d026094ad9faecb3d8c8985385b8e9ef9b42d9b
4zk <the4zk@hotmail.com>
2020-07-25 20:37:52 +0300

    New bunker module: Background Ladder

--- Base.rte/Scenes/Objects/Bunkers/BunkerSystems/Ladders/LadderNode.lua
+++ Base.rte/Scenes/Objects/Bunkers/BunkerSystems/Ladders/LadderNode.lua
@@ -1,8 +1,8 @@
 function Create(self)
 	self.checkTimer = Timer();
 	self.checkTimer:SetSimTimeLimitMS(51);
-	self.width = (ToMOSprite(self):GetSpriteWidth() / 2) - 1;
-	self.height = (ToMOSprite(self):GetSpriteHeight() / 2) - 1;
+	self.width = (ToMOSprite(self):GetSpriteWidth()/2) - 1;
+	self.height = (ToMOSprite(self):GetSpriteHeight()/2) - 1;
 end
 function Update(self)
 	if self.PinStrength > 0 and self.checkTimer:IsPastSimTimeLimit() then
@@ -11,7 +11,7 @@ function Update(self)
 		self.Vel, self.AngularVel = Vector(), 0;
 		local checkPos = self.Pos + Vector(self.width * self.FlipFactor, math.random(-self.height, self.height)):RadRotate(self.RotAngle);
 		local moCheck = SceneMan:GetMOIDPixel(checkPos.X, checkPos.Y);
-		if moCheck ~= 255 then
+		if moCheck ~= rte.NoMOID then
 			local mo = MovableMan:GetMOFromID(moCheck);
 			if IsAttachable(mo) and ToAttachable(mo):GetParent() and IsAHuman(ToAttachable(mo):GetParent()) then
 				local actor = ToAHuman(MovableMan:GetMOFromID(mo.RootID));
@@ -19,10 +19,10 @@ function Update(self)
 				if actor.Status == Actor.STABLE and actor.FlipFactor ~= self.FlipFactor and not controller:IsState(Controller.BODY_JUMP) then
 					actor.Vel = actor.Vel * (1 - 1 / velFactor);
 					if controller:IsState(Controller.MOVE_LEFT) or controller:IsState(Controller.MOVE_RIGHT) then
-						local speed = actor:GetLimbPathSpeed(1) / velFactor;
-						actor.Vel = actor.Vel + Vector(speed / 2, 0):RadRotate(actor:GetAimAngle(true)) - Vector(0, speed);
+						local speed = actor:GetLimbPathSpeed(1)/velFactor;
+						actor.Vel = actor.Vel + Vector(speed/2, 0):RadRotate(actor:GetAimAngle(true)) - Vector(0, speed);
 					elseif controller:IsState(Controller.BODY_CROUCH) then
-						actor.Vel = actor.Vel / 2;
+						actor.Vel = actor.Vel/2;
 					end	
 				end	
 			end	
