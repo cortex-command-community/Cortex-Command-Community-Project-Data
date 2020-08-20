1c3ac13ffdea432e9b4ac7952ae2ac5c3b4469ae
4zk <the4zk@hotmail.com>
2020-08-20 23:54:23 +0300

    Browncoats rework

---
+++ Browncoats.rte/Devices/Weapons/Flash/Missile.lua
@@ -0,0 +1,39 @@
+function Create(self)
+
+	self.turnStrength = 3;
+	self.shake = 0.5;
+	self.lifeTimer = Timer();
+	
+	if self:NumberValueExists("TargetID") and self:GetNumberValue("TargetID") ~= rte.NoMOID then
+		local mo = MovableMan:GetMOFromID(self:GetNumberValue("TargetID"));
+		if mo and IsActor(mo) then
+			self.target = ToActor(mo);
+		end
+	end
+	self.lifeTimer:SetSimTimeLimitMS(math.random(self.Lifetime/2, self.Lifetime - math.ceil(TimerMan.DeltaTimeMS)));
+	self.activationDelay = math.random(50, 100);
+end
+function Update(self)
+	self.GlobalAccScalar = 1/math.sqrt(1 + math.abs(self.Vel.X)/10);
+	if self.lifeTimer:IsPastSimMS(self.activationDelay) then
+		self:EnableEmission(true);
+		if self.target and self.target.ID ~= rte.NoMOID then
+			local targetDist = SceneMan:ShortestDistance(self.Pos, self.target.Pos, SceneMan.SceneWrapsX);
+			if targetDist.Magnitude < (self.Radius + self.target.Radius) then
+				self:GibThis();
+			else
+				local targetVel = targetDist:SetMagnitude(self.turnStrength);
+
+				local turnAngle = self.Vel.AbsRadAngle - targetVel.AbsRadAngle;
+				turnAngle = turnAngle > math.pi and turnAngle - (math.pi * 2) or (turnAngle < -math.pi and turnAngle + (math.pi * 2) or turnAngle);
+
+				self.Vel = (self.Vel + targetVel):SetMagnitude(self.Vel.Magnitude);
+				self.AngularVel = self.AngularVel/2 - (turnAngle * self.turnStrength);
+			end
+		end
+		self.Vel = Vector(self.Vel.X, self.Vel.Y):DegRotate(RangeRand(-self.shake, self.shake) * math.sqrt(1 + self.Vel.Magnitude));
+		if self.lifeTimer:IsPastSimTimeLimit() or (self.Vel.Magnitude + math.abs(self.AngularVel)) < 3 then
+			self:GibThis();
+		end
+	end
+end
