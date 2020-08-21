bc2bac7242d7937c1e543aa4785518b786e6c908
4zk <the4zk@hotmail.com>
2020-08-21 11:21:54 +0300

    Imperatus rework

---
+++ Imperatus.rte/Devices/Weapons/Bulldog/Bulldog.lua
@@ -0,0 +1,26 @@
+function Create(self)
+	self.origActivationDelay = self.ActivationDelay;
+	self.origDeactivationDelay = self.DeactivationDelay;
+	self.maxRateOfFire = self.RateOfFire;
+	self.minRateOfFire = self.maxRateOfFire/2;
+	self.RateOfFire = self.minRateOfFire;
+	self.increasePerShot = 1.05;
+	self.decreasePerFrame = 0.99;
+	self.spinTimer = Timer();
+end
+function Update(self)
+	if self.FiredFrame then
+		self.RateOfFire = math.min(self.RateOfFire * self.increasePerShot, self.maxRateOfFire);
+		self.ActivationDelay = 0;
+		self.DeactivationDelay = self.origDeactivationDelay * 9;
+		self.spinTimer:Reset();
+	else
+		if not self:IsActivated() then
+			self.RateOfFire = math.max(self.RateOfFire * self.decreasePerFrame, self.minRateOfFire);
+		end
+		if self.RoundInMagCount == 0 or self.spinTimer:IsPastSimMS(self.DeactivationDelay * 0.9) then
+			self.ActivationDelay = self.origActivationDelay;
+			self.DeactivationDelay = self.origDeactivationDelay;
+		end
+	end
+end
