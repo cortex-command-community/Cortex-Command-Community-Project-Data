08d5d60dd35e32203992926771f91d38b860800e
4zk <the4zk@hotmail.com>
2020-09-05 23:07:15 +0300

    Better rotation method for visible inventory

--- Base.rte/AI/HumanFunctions.lua
+++ Base.rte/AI/HumanFunctions.lua
@@ -122,19 +122,18 @@ function HumanFunctions.DoVisibleInventory(actor, showAll)
 					if showAll or item.Radius > largestItem then
 						largestItem = item.Radius;
 						heldCount = heldCount + 1;
+						local itemCount = math.sqrt(heldCount);
 
 						local actorBack = Vector(ToMOSprite(actor):GetSpriteWidth() + actor.SpriteOffset.X, ToMOSprite(actor):GetSpriteHeight() + actor.SpriteOffset.Y);
-						local stackX = item.Radius * 0.2 + math.sqrt(heldCount) - 0.5;
+						local stackX = item.Radius * 0.2 + itemCount - 0.5;
 						--Bigger actors carry weapons higher up, smaller weapons are carried lower down
 						local drawPos = actor.Pos + Vector((-actorBack.X * 0.5 - stackX) * actor.FlipFactor, -actorBack.Y * 0.75):RadRotate(actor.RotAngle);
-
-						local itemCount = math.sqrt(math.abs(heldCount - thrownCount));
 						--Display tall objects upright
 						local widthToHeightRatio = ToMOSprite(item):GetSpriteWidth()/ToMOSprite(item):GetSpriteHeight();
 						local tallAng = widthToHeightRatio > 1 and 1.57 or 0;
 
-						local tilt = (1/item.Radius) * widthToHeightRatio * actor.FlipFactor;
-						local rotAng = actor.RotAngle + tallAng - (itemCount - 1) * tilt + (actor.InventorySize - heldCount) * tilt * 0.5;
+						local tilt = (itemCount/item.Radius) * widthToHeightRatio * actor.FlipFactor;
+						local rotAng = actor.RotAngle + tallAng + (tilt * 2) - tilt * (itemCount - 1);
 
 						for player = Activity.PLAYER_1, Activity.MAXPLAYERCOUNT - 1 do
 							local screen = ActivityMan:GetActivity():ScreenOfPlayer(player);
