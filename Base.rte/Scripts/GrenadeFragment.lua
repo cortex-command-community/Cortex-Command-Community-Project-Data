43fd1d1e4b07796522e3e61577a02f744428e0fc
4zk <the4zk@hotmail.com>
2020-09-19 18:13:18 +0300

    New scripted grenade fragment particle + test application

---
+++ Base.rte/Scripts/GrenadeFragment.lua
@@ -0,0 +1,8 @@
+function Create(self)
+	self.skipFrames = math.random(2);
+end
+function Update(self)
+	if not self.HitsMOs and math.floor(self.Age/TimerMan.DeltaTimeMS + 0.5) >= self.skipFrames then
+		self.HitsMOs = true;
+	end
+end
