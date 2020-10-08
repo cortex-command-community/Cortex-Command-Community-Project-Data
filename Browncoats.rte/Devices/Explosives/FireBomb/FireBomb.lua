af182463555f07aaed4bc89bb05105385b14f8e3
4zk <the4zk@hotmail.com>
2020-10-08 21:39:16 +0300

    Re-implement PEmitter flame particle now that it is supported by lua + other flame-related tweaks

--- Browncoats.rte/Devices/Explosives/FireBomb/FireBomb.lua
+++ Browncoats.rte/Devices/Explosives/FireBomb/FireBomb.lua
@@ -4,6 +4,7 @@ function Create(self)
 	self.lifeTimer = Timer();
 	self.emitTimerA = Timer();
 	self.emitTimerB = Timer();
+	self.emitSpread = math.pi * 2;
 
 	self.smallAngle = math.pi/6;
 
@@ -51,32 +52,32 @@ function Update(self)
 
 				local fireA = CreateMOPixel("Particle Browncoat Incendiary Flame 1");
 				fireA.Pos = self.Pos;
-				fireA.Vel = self.Vel/2 + Vector(math.random(10, 15), 0):RadRotate(randomAngleA);
+				fireA.Vel = self.Vel * 0.5 + Vector(math.random(10, 15), 0):RadRotate(randomAngleA);
 				MovableMan:AddParticle(fireA);
 
 				local fireB = CreateMOPixel("Particle Browncoat Incendiary Flame 2");
 				fireB.Pos = self.Pos;
-				fireB.Vel = self.Vel/2 + Vector(math.random(5, 10), 0):RadRotate(randomAngleB);
+				fireB.Vel = self.Vel * 0.5 + Vector(math.random(5, 10), 0):RadRotate(randomAngleB);
 				MovableMan:AddParticle(fireB);
 
 				local fireC = CreateMOSParticle("Tiny Smoke Ball Incendiary Flamer 1");
 				fireC.Pos = self.Pos;
-				fireC.Vel = self.Vel/2 + Vector(math.random(2, 5), 0):RadRotate(randomAngleC);
+				fireC.Vel = self.Vel * 0.5 + Vector(math.random(2, 5), 0):RadRotate(randomAngleC);
 				MovableMan:AddParticle(fireC);
 			else
 				local fireA = CreateMOPixel("Particle Browncoat Incendiary Flame 1");
 				fireA.Pos = self.Pos;
-				fireA.Vel = self.Vel/2 + Vector(math.random(10, 15), 0):RadRotate(math.random() * 6.283);
+				fireA.Vel = self.Vel * 0.5 + Vector(math.random(10, 15), 0):RadRotate(math.random() * self.emitSpread);
 				MovableMan:AddParticle(fireA);
 
 				local fireB = CreateMOPixel("Particle Browncoat Incendiary Flame 2");
 				fireB.Pos = self.Pos;
-				fireB.Vel = self.Vel/2 + Vector(math.random(5, 10), 0):RadRotate(math.random() * 6.283);
+				fireB.Vel = self.Vel * 0.5 + Vector(math.random(5, 10), 0):RadRotate(math.random() * self.emitSpread);
 				MovableMan:AddParticle(fireB);
 
 				local fireC = CreateMOSParticle("Tiny Smoke Ball Incendiary Flamer 1");
 				fireC.Pos = self.Pos;
-				fireC.Vel = self.Vel/2 + Vector(math.random(2, 5), 0):RadRotate(math.random() * 6.283);
+				fireC.Vel = self.Vel * 0.5 + Vector(math.random(2, 5), 0):RadRotate(math.random() * self.emitSpread);
 				MovableMan:AddParticle(fireC);
 			end
 
@@ -93,28 +94,28 @@ function Update(self)
 
 				local fireD = CreateMOSParticle("Tiny Smoke Ball Incendiary Flamer 2");
 				fireD.Pos = self.Pos;
-				fireD.Vel = self.Vel/2 + Vector(math.random(10, 15), 0):RadRotate(randomAngleA);
+				fireD.Vel = self.Vel * 0.5 + Vector(math.random(10, 15), 0):RadRotate(randomAngleA);
 				MovableMan:AddParticle(fireD);
 
 				local fireE = CreateMOSParticle("Tiny Smoke Ball Incendiary Flamer 3");
 				fireE.Pos = self.Pos;
-				fireE.Vel = self.Vel/2 + Vector(math.random(5, 10), 0):RadRotate(randomAngleB);
+				fireE.Vel = self.Vel * 0.5 + Vector(math.random(5, 10), 0):RadRotate(randomAngleB);
 				MovableMan:AddParticle(fireE);
 				
-				local flame = CreateMOSParticle("Flame Hurt Short Float");
+				local flame = CreatePEmitter("Flame Hurt Short Float");
 				flame.Pos = self.Pos;
-				flame.Vel = self.Vel/2 + Vector(math.random(3, 7), 0):RadRotate(math.random() * 6.283);
+				flame.Vel = self.Vel * 0.5 + Vector(math.random(3, 7), 0):RadRotate(math.random() * self.emitSpread);
 				flame.Lifetime = flame.Lifetime * RangeRand(0.5, 1.0);
 				MovableMan:AddParticle(flame);
 			else
 				local fireD = CreateMOSParticle("Tiny Smoke Ball Incendiary Flamer 2");
 				fireD.Pos = self.Pos;
-				fireD.Vel = self.Vel/2 + Vector(math.random(10, 15), 0):RadRotate(math.random() * 6.283);
+				fireD.Vel = self.Vel * 0.5 + Vector(math.random(10, 15), 0):RadRotate(math.random() * self.emitSpread);
 				MovableMan:AddParticle(fireD);
 
 				local fireE = CreateMOSParticle("Tiny Smoke Ball Incendiary Flamer 3");
 				fireE.Pos = self.Pos;
-				fireE.Vel = self.Vel/2 + Vector(math.random(5, 10), 0):RadRotate(math.random() * 6.283);
+				fireE.Vel = self.Vel * 0.5 + Vector(math.random(5, 10), 0):RadRotate(math.random() * self.emitSpread);
 				MovableMan:AddParticle(fireE);
 			end
 		end
