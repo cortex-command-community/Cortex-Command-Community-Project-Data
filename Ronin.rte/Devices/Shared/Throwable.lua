82b976fbd62b1f4a014e3ce5eee3e22686998dbc
4zk <the4zk@hotmail.com>
2020-10-08 21:39:30 +0300

    Bottle doesn't seem to break :/

--- Ronin.rte/Devices/Shared/Throwable.lua
+++ Ronin.rte/Devices/Shared/Throwable.lua
@@ -1,5 +1,6 @@
 function Create(self)
 	self.origMass = self.Mass;
+	self.origGibImpulseLimit = self.GibImpulseLimit;
 	self.thrownMassMultiplier = self:NumberValueExists("ThrownMassMultiplier") and self:GetNumberValue("ThrownMassMultiplier") or 5;
 end
 function Update(self)
@@ -7,7 +8,7 @@ function Update(self)
 		if not self.thrown then
 			self.AngularVel = self.AngularVel - self.Vel.Magnitude * self.FlipFactor * math.random();
 			self.Mass = self.origMass * self.thrownMassMultiplier;
-			self.GibImpulseLimit = self.GibImpulseLimit * self.thrownMassMultiplier;
+			self.GibImpulseLimit = self.origGibImpulseLimit * self.thrownMassMultiplier;
 			self.thrown = true;
 		end
 	else
