9d56025710932d8dc8b679c6ba94e378d5ae181e
fourZK <the4zk@hotmail.com>
2021-02-11 21:29:57 +0200

    Animated ladder node

--- Base.rte/Scenes/Objects/Bunkers/BunkerSystems/Ladders/LadderNode.lua
+++ Base.rte/Scenes/Objects/Bunkers/BunkerSystems/Ladders/LadderNode.lua
@@ -1,8 +1,8 @@
 function Create(self)
 	self.checkTimer = Timer();
 	self.checkTimer:SetSimTimeLimitMS(51);
-	self.width = (ToMOSprite(self):GetSpriteWidth()/2) - 1;
-	self.height = (ToMOSprite(self):GetSpriteHeight()/2) - 1;
+	self.width = (ToMOSprite(self):GetSpriteWidth() * 0.5) - 1;
+	self.height = (ToMOSprite(self):GetSpriteHeight() * 0.5) - 1;
 end
 function Update(self)
 	if self.PinStrength > 0 and self.checkTimer:IsPastSimTimeLimit() then
@@ -21,12 +21,13 @@ function Update(self)
 					actor.Vel = actor.Vel * (1 - 1/velFactor);
 					if controller:IsState(Controller.MOVE_LEFT) or controller:IsState(Controller.MOVE_RIGHT) then
 						local speed = actor:GetLimbPathSpeed(1)/velFactor;
-						actor.Vel = actor.Vel + Vector(speed/2, 0):RadRotate(actor:GetAimAngle(true)) - Vector(0, speed);
+						actor.Vel = actor.Vel + Vector(speed * 0.5, 0):RadRotate(actor:GetAimAngle(true)) - Vector(0, speed);
 					elseif controller:IsState(Controller.BODY_CROUCH) then
-						actor.Vel = actor.Vel/2;
-					end	
-				end	
-			end	
-		end	
-	end	
+						actor.Vel = actor.Vel * 0.5;
+					end
+					self.Frame = (self.Frame + 1) % self.FrameCount;
+				end
+			end
+		end
+	end
 end
