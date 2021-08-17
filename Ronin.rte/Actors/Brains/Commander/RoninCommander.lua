5de1085c74eed425937f1268f0eca4406ac39842
fourZK <the4zk@hotmail.com>
2021-08-17 17:10:10 +0300

    A heap of Ronin edits

--- Ronin.rte/Actors/Brains/Commander/RoninCommander.lua
+++ Ronin.rte/Actors/Brains/Commander/RoninCommander.lua
@@ -2,7 +2,6 @@ function Create(self)
 	self.updateTimer = Timer();
 
 	self.lungeTapDelay = 200;
-	self.frontFlip = {input = {Controller.BODY_JUMP, Controller.BODY_CROUCH}, inputCounter  = 0};
 	self.lungePower = 6;
 	self.tapTimer = Timer();
 
@@ -13,6 +12,8 @@ function Create(self)
 		self:SetNumberValue("Identity", self.face);
 		--Add a ponytail if we are Lara
 		if self.face == 2 then
+			self.DeathSound.Pitch = 1.2;
+			self.PainSound.Pitch = 1.2;
 			self.Head:AddAttachable(CreateAttachable("Ronin Brunette Ponytail"));
 		end
 	else
@@ -21,8 +22,11 @@ function Create(self)
 			self.Head.Frame = self.face;
 		end
 	end
+	self.analogSpin = 0;
+	self.lastAnalogAngle = self:GetController().AnalogMove.AbsRadAngle;
 end
 function Update(self)
+	self.analogSpin = self.analogSpin * 0.9;
 	if self.updateTimer:IsPastSimMS(1000) then
 		self.updateTimer:Reset();
 		self.aggressive = self.Health < self.MaxHealth * 0.5;
@@ -34,49 +38,34 @@ function Update(self)
 		end
 	end
 	self.Frame = (self.face == 2 and self.Vel.Y > (SceneMan.GlobalAcc.Y * 0.15)) and 3 or self.face;
-	local crouching = self.controller:IsState(Controller.BODY_CROUCH);
 	
-	if self:IsPlayerControlled() and self.Status < Actor.DYING and self.FGLeg and self.BGLeg then
+	if self:IsPlayerControlled() and self.Status < Actor.INACTIVE and self.FGLeg and self.BGLeg then
+		local crouching = self.controller:IsState(Controller.BODY_CROUCH);
 		if self.Status == Actor.UNSTABLE then
-			if not crouching then
-				local motion = self.Vel.Magnitude * 0.5 + math.abs(self.AngularVel);
-				if motion > 1 then
-					self.AngularVel = self.AngularVel * (1 - 0.1/motion);
-				end
-				if self.tapTimer:IsPastSimMS(500) and math.cos(self.RotAngle) > 0.9 and motion < 20  then
+			local motion = (self.Vel.Magnitude * 0.5 + math.abs(self.AngularVel));
+			local stillness = 1/(1 + motion);
+			if crouching then
+				self.AngularVel = self.AngularVel * (1 - stillness) - (self.RotAngle - (self:GetAimAngle(false) - math.pi * 0.5) * self.FlipFactor) * 2 * stillness;
+			end
+		elseif crouching then
+			if not self.crouchHeld then
+				if not self.tapTimer:IsPastSimMS(self.lungeTapDelay) and SceneMan.Scene.GlobalAcc.Magnitude > 10 then
+					Lunge(self, 3);
+				else
 					self.tapTimer:Reset();
-					self.Status = Actor.STABLE;
-					self.AngularVel = self.AngularVel * 0.5;
-				end
+				end	
+				self.controller:SetState(Controller.BODY_CROUCH, false);
 			end
 		else
-			--Track the key combo input (jump -> crouch) to trigger frontflip
-			if self.controller:IsState(self.frontFlip.input[self.frontFlip.inputCounter + 1]) then
-				if (self.frontFlip.inputCounter == 0 or not self.tapTimer:IsPastSimMS(self.lungeTapDelay)) then
-					self.frontFlip.inputCounter = self.frontFlip.inputCounter + 1;
-					if self.frontFlip.inputCounter == #self.frontFlip.input then
-						Lunge(self, 8);
-						self.frontFlip.inputCounter = 0;
-						--AudioMan:PlaySound("Ronin.rte/Actors/Brains/Commander/Sounds/Jump.flac", self.Pos);
-					end
-					self.tapTimer:Reset();
-				else
-					self.frontFlip.inputCounter = 0;
-				end
-			elseif crouching then
-				if not self.keyHeld then
-					if not self.tapTimer:IsPastSimMS(self.lungeTapDelay) and SceneMan.Scene.GlobalAcc.Magnitude > 10 then
-						Lunge(self, 0);
-					else
-						self.tapTimer:Reset();
-					end	
-				end
-				self.keyHeld = true;
-			else
-				self.keyHeld = false;
+			self.analogSpin = self.analogSpin + math.sin(self.controller.AnalogMove.AbsRadAngle - self.lastAnalogAngle);
+			if math.abs(self.analogSpin) > 2 then
+				Lunge(self, 12);
+				self.analogSpin = 0;
 			end
 		end
+		self.crouchHeld = crouching;
 	end
+	self.lastAnalogAngle = self.controller.AnalogMove.AbsRadAngle;
 end
 
 function Lunge(self, spin)
@@ -87,8 +76,8 @@ function Lunge(self, spin)
 		flip = -1;
 	end
 	--Different factors that affect the lunge
-	local vel = (self.Vel.Magnitude^2) * 0.0005 + 1;
-	local ang = math.abs(self.AngularVel * 0.05) + 1;
+	local angVel = math.abs(self.AngularVel * 0.1) + 1;
+	local vel = (self.Vel.Magnitude + angVel)^2 * 0.0005 + 1;
 	local mass = math.abs(self.Mass * 0.005) + 1;
 	local aimAng = self:GetAimAngle(false);
 	local vertical = math.abs(math.cos(aimAng))/vel;
@@ -97,7 +86,7 @@ function Lunge(self, spin)
 	local jumpVec =	Vector((self.lungePower + strength/vel) * flip, -(self.lungePower * 0.5 + (strength * 0.3)) * vertical):RadRotate(aimAng * self.FlipFactor);
 	
 	self.Vel = self.Vel + jumpVec/mass;
-	self.AngularVel = self.AngularVel - (2/ang * vertical + spin) * flip;
+	self.AngularVel = self.AngularVel - (spin/angVel * vertical) * flip * math.cos(self.RotAngle);
 	self.Status = Actor.UNSTABLE;
 	self.tapTimer:Reset();
 end
