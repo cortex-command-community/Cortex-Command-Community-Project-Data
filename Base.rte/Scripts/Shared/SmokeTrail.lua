f614a00cb95c47b8bb1990a0a7a87b1874a7573c
fourZK <the4zk@hotmail.com>
2021-09-05 14:00:15 +0300

    Enhancements to shared device scripts TrajectoryGuide and SmokeTrail

--- Base.rte/Scripts/Shared/SmokeTrail.lua
+++ Base.rte/Scripts/Shared/SmokeTrail.lua
@@ -1,5 +1,6 @@
 function Create(self)
 	self.smokeTrailLifeTime = self:NumberValueExists("SmokeTrailLifeTime") and self:GetNumberValue("SmokeTrailLifeTime") or 150;
+	self.smokeTrailSize = self:NumberValueExists("SmokeTrailSize") and self:GetNumberValue("SmokeTrailSize") or 2;
 	self.smokeTrailRadius = self:NumberValueExists("SmokeTrailRadius") and self:GetNumberValue("SmokeTrailRadius") or ToMOSprite(self):GetSpriteHeight() * 0.5 - 1;
 	self.smokeTrailTwirl = self:NumberValueExists("SmokeTrailTwirl") and self:GetNumberValue("SmokeTrailTwirl") or 0;
 	
@@ -9,20 +10,21 @@ end
 function Update(self)
 	local offset = self.Vel * rte.PxTravelledPerFrame;	--The effect will be created the next frame so move it one frame backwards towards the barrel
 	
-	local trailLength = math.floor(offset.Magnitude * 0.5 - 1);
+	local trailLength = math.floor(offset.Magnitude/self.smokeTrailSize - self.smokeTrailSize);
+	local setVel = Vector(self.Vel.X, self.Vel.Y):SetMagnitude(math.sqrt(self.Vel.Magnitude));
 	for i = 1, trailLength do
-		local effect = CreateMOSParticle("Tiny Smoke Trail 1", "Base.rte");
+		local effect = self.smokeTrailSize < 2 and CreateMOPixel("Micro Smoke Trail " .. math.random(3), "Base.rte") or CreateMOSParticle((self.smokeTrailSize < 3 and "Tiny" or "Small") .. " Smoke Trail " .. math.random(3), "Base.rte");
 		effect.Pos = self.Pos - (offset * i/trailLength) + Vector(RangeRand(-1, 1), RangeRand(-1, 1)) * self.smokeTrailRadius;
-		effect.Vel = self.Vel * RangeRand(0.75, 1);
-		effect.Lifetime = self.smokeTrailLifeTime * RangeRand(0.5, 1) * (self.Lifetime > 1 and 1 - self.Age/self.Lifetime or 1);
-		effect.AirResistance = effect.AirResistance * RangeRand(0.9, 1);
+		effect.Vel = setVel * RangeRand(0.6, 1);
+		effect.Lifetime = self.smokeTrailLifeTime * RangeRand(0.4, 1) * (self.Lifetime > 1 and 1 - self.Age/self.Lifetime or 1);
+		effect.AirResistance = effect.AirResistance * RangeRand(0.8, 1);
 		effect.AirThreshold = self.smokeAirThreshold;
 	
 		if self.smokeTrailTwirl > 0 then
 			effect.GlobalAccScalar = effect.GlobalAccScalar * math.random();
 
 			effect.Pos = self.Pos - offset + (offset * i/trailLength);
-			effect.Vel = self.Vel + Vector(0, math.sin(self.smokeTwirlCounter) * self.smokeTrailTwirl + RangeRand(-0.1, 0.1)):RadRotate(self.Vel.AbsRadAngle);
+			effect.Vel = setVel + Vector(0, math.sin(self.smokeTwirlCounter) * self.smokeTrailTwirl + RangeRand(-0.1, 0.1)):RadRotate(self.Vel.AbsRadAngle);
 			
 			self.smokeTwirlCounter = self.smokeTwirlCounter + RangeRand(-0.2, 0.4);
 		end
