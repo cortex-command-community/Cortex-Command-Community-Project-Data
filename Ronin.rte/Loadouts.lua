c31e00bcb2deaa3d4fe48f8f331c9cf331821046
Gareth YR <garethrickwood@hotmail.com>
2022-12-06 14:59:03 -0400

    Fiddled with all the loadouts
    1. They're a little more consistent in equipment and craft selections
    2. There are more medikits
    3. Engineers will always have breaching tools and reasonable diggers
    4. Swarm factions (except ronin) will generally have more troops in loadouts

--- Ronin.rte/Loadouts.lua
+++ Ronin.rte/Loadouts.lua
@@ -1,11 +1,11 @@
 --The following is a list of loadout-specific weapon sets that are drawn at random when deploying a Ronin from Loadouts
 --Primary, Secondary and Tertiary weapons are always of class HDFirearm
-RoninLoadouts = {["Rifleman"] = {}, ["Machinegunner"] = {}, ["CQB"] = {}, ["Grenadier"] = {}, ["Sniper"] = {}, ["Engineer"] = {}};
+RoninLoadouts = {["Rifleman"] = {}, ["Machinegunner"] = {}, ["CQB"] = {}, ["Grenadier"] = {}, ["Sniper"] = {}, ["Engineer"] = {}, ["Medic"] = {}};
 
 RoninLoadouts["Rifleman"]["Primary"] = {"AK-47", "M16A2"};
 RoninLoadouts["Rifleman"]["Secondary"] = {".357 Magnum", "Desert Eagle"};
-RoninLoadouts["Rifleman"]["Tertiary"] = {"Luger P08", "Beretta 93R", "Shovel", "Medical Dart Gun", "Grapple Gun", "Medikit"};
-RoninLoadouts["Rifleman"]["Throwable"] = {"M67 Grenade Belt", "M24 Potato Masher Belt", "Molotov Cocktail Belt", "Scrambler Belt"};
+RoninLoadouts["Rifleman"]["Tertiary"] = {"Luger P08", "Beretta 93R", "Shovel", "Grapple Gun", "Medikit"};
+RoninLoadouts["Rifleman"]["Throwable"] = {"M67 Grenade Belt", "M24 Potato Masher Belt", "Molotov Cocktail Belt"};
 
 RoninLoadouts["Machinegunner"]["Headgear"] = {"Soldier Helmet", "Motorcycle Helmet", "Dummy Mask", "Browncoat Mask"};
 RoninLoadouts["Machinegunner"]["Primary"] = {"M60", "Stoner 63"};
@@ -15,6 +15,7 @@ RoninLoadouts["Machinegunner"]["Tertiary"] = {"Luger P08", "Beretta 93R", "Shove
 RoninLoadouts["CQB"]["Headgear"] = {"Dummy Mask", "Browncoat Mask"};
 RoninLoadouts["CQB"]["Primary"] = {"Model 590", "SPAS 12"};
 RoninLoadouts["CQB"]["Secondary"] = {".357 Magnum", "Desert Eagle", "UZI", "MP5K", "Sawed-Off Shotgun", "Chainsaw"};
+RoninLoadouts["CQB"]["Tertiary"] = {".357 Magnum", "Desert Eagle", "UZI", "MP5K", "Riot Shield", "Medikit"};
 RoninLoadouts["CQB"]["Throwable"] = {"Molotov Cocktail Belt", "Scrambler Belt"};
 
 RoninLoadouts["Grenadier"]["Headgear"] = {"Soldier Helmet"};
@@ -25,9 +26,15 @@ RoninLoadouts["Grenadier"]["Throwable"] = {"M67 Grenade", "M24 Potato Masher Bel
 RoninLoadouts["Sniper"]["Headgear"] = {"Sniper Hat"};
 RoninLoadouts["Sniper"]["Primary"] = {"Kar98k", "M1 Garand"};
 RoninLoadouts["Sniper"]["Secondary"] = {"UZI", "MP5K"};
-RoninLoadouts["Sniper"]["Throwable"] = {"Empty Bottle Belt"};
+RoninLoadouts["Sniper"]["Tertiary"] = {"Medikit", "Light Digger"};
+RoninLoadouts["Sniper"]["Throwable"] = {"Empty Bottle Belt", "Stone Bag"};
 
 RoninLoadouts["Engineer"]["Headgear"] = {"Soldier Helmet"};
 RoninLoadouts["Engineer"]["Primary"] = {"UZI", "MP5K"};
-RoninLoadouts["Engineer"]["Secondary"] = {"Shovel", "Light Digger"};
-RoninLoadouts["Engineer"]["Throwable"] = {"Scrambler Belt"};
+RoninLoadouts["Engineer"]["Secondary"] = {"Medium Digger", "Medium Digger", "Shovel"};
+RoninLoadouts["Engineer"]["Throwable"] = {"Remote Explosive", "Remote Explosive", "Scrambler Belt"};
+
+RoninLoadouts["Medic"]["Headgear"] = {"Soldier Helmet"};
+RoninLoadouts["Medic"]["Primary"] = {"UZI", "MP5K"};
+RoninLoadouts["Medic"]["Secondary"] = {"Medical Dart Gun"};
+RoninLoadouts["Medic"]["Tertiary"] = {"Medikit"};
