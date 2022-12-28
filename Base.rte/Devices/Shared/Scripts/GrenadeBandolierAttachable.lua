33261c010929f223eb14116b385f73672978be95
MaximDude <maximdude@gmail.com>
2022-12-28 05:55:03 +0200

    Rename belts to bandoliers

--- Base.rte/Devices/Shared/Scripts/GrenadeBandolierAttachable.lua
+++ Base.rte/Devices/Shared/Scripts/GrenadeBandolierAttachable.lua
@@ -2,14 +2,14 @@ function Create(self)
 
 	self.grenadeName = self:GetStringValue("Grenade Name");
 	self.grenadeTech = self:GetStringValue("Grenade Tech");
-	
+
 	if self:NumberValueExists("Thrown Device") then
 		self.grenadeObject = CreateThrownDevice(self.grenadeName, self.grenadeTech);
 	else
 		self.grenadeObject = CreateTDExplosive(self.grenadeName, self.grenadeTech);
 	end
 
-	self.grenadeBeltName = self:GetStringValue("Belt Name");
+	self.grenadeBandolierName = self:GetStringValue("Bandolier Name");
 
 	self.addedGrenade = false;
 	self.addGrenadeTimer = Timer();
@@ -21,30 +21,30 @@ function Create(self)
 
 	if self.rootParent then
 		self.rootParent:EquipNamedDevice(self.grenadeName, true);
-	
-		if self.rootParent:HasObject(self.grenadeBeltName) then
-			self.rootParent:RemoveInventoryItem(self.grenadeBeltName);
+
+		if self.rootParent:HasObject(self.grenadeBandolierName) then
+			self.rootParent:RemoveInventoryItem(self.grenadeBandolierName);
 		end
 
-		self.grenadePerBelt = self:GetNumberValue("Grenade Count");
-		self.rootParent:SetNumberValue(self.grenadeBeltName, self.grenadePerBelt - 1);
-		self.grenadeCount = self.rootParent:GetNumberValue(self.grenadeBeltName);	--Get ammount of grenades in our belt
+		self.grenadePerBandolier = self:GetNumberValue("Grenade Count");
+		self.rootParent:SetNumberValue(self.grenadeBandolierName, self.grenadePerBandolier - 1);
+		self.grenadeCount = self.rootParent:GetNumberValue(self.grenadeBandolierName);	--Get ammount of grenades in our Bandolier
 
-		self.beltMass = self:GetNumberValue("Belt Mass");
+		self.BandolierMass = self:GetNumberValue("Bandolier Mass");
 
 		self.explosiveGoldValue = self:GetNumberValue("Grenade Value");
-		self.rootParent:SetGoldValue(self.rootParent:GetGoldValue(self.grenadeObject.ModuleID, 1, 1) + (self.explosiveGoldValue * (self.grenadePerBelt - 1)));
+		self.rootParent:SetGoldValue(self.rootParent:GetGoldValue(self.grenadeObject.ModuleID, 1, 1) + (self.explosiveGoldValue * (self.grenadePerBandolier - 1)));
 
 		self.IsPlayer = ActivityMan:GetActivity():IsHumanTeam(self.rootParent.Team)
-		
+
 	end
-	
+
 	-- Icons go here, loaded in Create for efficiency
 	self.grenadeAmmoIcon = CreateMOSParticle("Ammo Icon", "Base.rte");
 	self.grenadeRefreshIcon = CreateTDExplosive(self.grenadeName, self.grenadeTech);
 	-- TODO maybe change sprite or at least sprite colour for refresh plus
 	self.grenadeRefreshPlus = CreateMOSParticle("Particle Heal Effect", "Base.rte");
-	
+
 	self.refreshGui = false;
 	self.refreshGuiTimer = Timer();
 	self.refreshGuiDelay = 1200;
@@ -57,58 +57,58 @@ function Update(self)
 	end
 
 	if self.rootParent and self.rootParent.Health > 0 then
-		if self.rootParent:HasObject(self.grenadeBeltName) then
-			self.rootParent:RemoveInventoryItem(self.grenadeBeltName);
-			self.rootParent:SetNumberValue(self.grenadeBeltName, self.grenadeCount+self.grenadePerBelt);
-			self.rootParent:SetGoldValue(self.rootParent:GetGoldValue(self.rootParent.ModuleID, 1, 1) + self.explosiveGoldValue*self.grenadePerBelt);
-		end	
-		
-		self.grenadeCount = self.rootParent:GetNumberValue(self.grenadeBeltName);
+		if self.rootParent:HasObject(self.grenadeBandolierName) then
+			self.rootParent:RemoveInventoryItem(self.grenadeBandolierName);
+			self.rootParent:SetNumberValue(self.grenadeBandolierName, self.grenadeCount+self.grenadePerBandolier);
+			self.rootParent:SetGoldValue(self.rootParent:GetGoldValue(self.rootParent.ModuleID, 1, 1) + self.explosiveGoldValue*self.grenadePerBandolier);
+		end
 
-		self.Mass = self.beltMass + self:GetNumberValue("Grenade Mass") * self.grenadeCount;
-		
+		self.grenadeCount = self.rootParent:GetNumberValue(self.grenadeBandolierName);
+
+		self.Mass = self.BandolierMass + self:GetNumberValue("Grenade Mass") * self.grenadeCount;
+
 		if self.grenadeCount <= 0 then
-			if self.rootParent:HasObject(self.grenadeName) then	
+			if self.rootParent:HasObject(self.grenadeName) then
 				self.Mass = 0;
 			else
 				self.ToDelete = true;
 			end
 		end
-		
+
 		if self.rootParent.HUDVisible and self.rootParent.EquippedItem and self.rootParent.EquippedItem.PresetName == self.grenadeName and self.rootParent:IsPlayerControlled() and not (self.rootParent.Jetpack and self.rootParent.Jetpack:IsEmitting()) then
 			local rootParentController = self.rootParent:GetController();
 			local distanceBetweenIconAndText = 2 + math.floor(self.grenadeAmmoIcon:GetSpriteWidth() * 0.5);
 			local drawPosition = self.rootParent.AboveHUDPos + Vector(-distanceBetweenIconAndText, rootParentController:IsState(Controller.PIE_MENU_ACTIVE) and 16 or 2);
-	
+
 			PrimitiveMan:DrawBitmapPrimitive(rootParentController.Player, drawPosition, self.grenadeAmmoIcon, 3.14, 0, true, true);
 			drawPosition = drawPosition + Vector(distanceBetweenIconAndText, -self.grenadeAmmoIcon:GetSpriteHeight() * 0.5);
 			PrimitiveMan:DrawTextPrimitive(rootParentController.Player, drawPosition, tostring(self.grenadeCount + 1), true, 0);
 		end
-		
-		if self.rootParent:HasObject(self.grenadeName) or (self.rootParent.EquippedItem and self.rootParent.EquippedItem.PresetName == self.grenadeName) then	
+
+		if self.rootParent:HasObject(self.grenadeName) or (self.rootParent.EquippedItem and self.rootParent.EquippedItem.PresetName == self.grenadeName) then
 			self.addGrenadeTimer:Reset();
 		elseif self.addGrenadeTimer:IsPastSimMS(self.grenadeReplenishDelay) and self.grenadeCount > 0 then
 			self.rootParent:AddInventoryItem(self.grenadeObject:Clone());
-			
+
 			if self.grenadeReplenishDelay < 100 then
 				self.rootParent:EquipNamedDevice(self.grenadeName, true);
 			else
 				self.refreshGui = true;
-				self.refreshGuiTimer:Reset();			
+				self.refreshGuiTimer:Reset();
 			end
-			
-			self.rootParent:SetNumberValue(self.grenadeBeltName, self.grenadeCount - 1);
+
+			self.rootParent:SetNumberValue(self.grenadeBandolierName, self.grenadeCount - 1);
 			self.rootParent:SetGoldValue(self.rootParent:GetGoldValue(self.rootParent.ModuleID, 1, 1) - self.explosiveGoldValue);
 
 		end
-		
+
 		if self.refreshGui == true then
 			if self.IsPlayer == true and self.HUDVisible == true then
 				local rootParentController = self.rootParent:GetController();
-							
+
 				local grenadeRefreshIconPos = self.rootParent.AboveHUDPos + Vector(25, 24);
 				local grenadeRefreshPlusPos = self.rootParent.AboveHUDPos + Vector(30, 24);
-								
+
 				PrimitiveMan:DrawBitmapPrimitive(rootParentController.Player, grenadeRefreshIconPos, self.grenadeRefreshIcon, 3.14, 0, true, true);
 				PrimitiveMan:DrawBitmapPrimitive(rootParentController.Player, grenadeRefreshPlusPos, self.grenadeRefreshPlus, 3.14, 0, true, true);
 			end
@@ -117,6 +117,6 @@ function Update(self)
 			end
 		else
 			self.refreshGuiTimer:Reset();
-		end	
-	end	
+		end
+	end
 end
--- Ronin.rte/Loadouts.lua
+++ Ronin.rte/Loadouts.lua
@@ -5,7 +5,7 @@ RoninLoadouts = {["Rifleman"] = {}, ["Machinegunner"] = {}, ["CQB"] = {}, ["Gren
 RoninLoadouts["Rifleman"]["Primary"] = {"AK-47", "M16A2"};
 RoninLoadouts["Rifleman"]["Secondary"] = {".357 Magnum", "Desert Eagle"};
 RoninLoadouts["Rifleman"]["Tertiary"] = {"Luger P08", "Beretta 93R", "Shovel", "Grapple Gun", "Medikit"};
-RoninLoadouts["Rifleman"]["Throwable"] = {"M67 Grenade Belt", "M24 Potato Masher Belt", "Molotov Cocktail Belt"};
+RoninLoadouts["Rifleman"]["Throwable"] = {"M67 Grenade Bandolier", "M24 Potato Masher Bandolier", "Molotov Cocktail Bandolier"};
 
 RoninLoadouts["Machinegunner"]["Headgear"] = {"Soldier Helmet", "Motorcycle Helmet", "Dummy Mask", "Browncoat Mask"};
 RoninLoadouts["Machinegunner"]["Primary"] = {"M60", "Stoner 63"};
@@ -16,23 +16,23 @@ RoninLoadouts["CQB"]["Headgear"] = {"Dummy Mask", "Browncoat Mask"};
 RoninLoadouts["CQB"]["Primary"] = {"Model 590", "SPAS 12"};
 RoninLoadouts["CQB"]["Secondary"] = {".357 Magnum", "Desert Eagle", "UZI", "MP5K", "Sawed-Off Shotgun", "Chainsaw"};
 RoninLoadouts["CQB"]["Tertiary"] = {".357 Magnum", "Desert Eagle", "UZI", "MP5K", "Riot Shield", "Medikit"};
-RoninLoadouts["CQB"]["Throwable"] = {"Molotov Cocktail Belt", "Scrambler Belt"};
+RoninLoadouts["CQB"]["Throwable"] = {"Molotov Cocktail Bandolier", "Scrambler Bandolier"};
 
 RoninLoadouts["Grenadier"]["Headgear"] = {"Soldier Helmet"};
 RoninLoadouts["Grenadier"]["Primary"] = {"RPG-7", "M79", "RPC M17"};
 RoninLoadouts["Grenadier"]["Secondary"] = {"UZI", "MP5K"};
-RoninLoadouts["Grenadier"]["Throwable"] = {"M67 Grenade", "M24 Potato Masher Belt"};
+RoninLoadouts["Grenadier"]["Throwable"] = {"M67 Grenade", "M24 Potato Masher Bandolier"};
 
 RoninLoadouts["Sniper"]["Headgear"] = {"Sniper Hat"};
 RoninLoadouts["Sniper"]["Primary"] = {"Kar98k", "M1 Garand"};
 RoninLoadouts["Sniper"]["Secondary"] = {"UZI", "MP5K"};
 RoninLoadouts["Sniper"]["Tertiary"] = {"Medikit", "Light Digger"};
-RoninLoadouts["Sniper"]["Throwable"] = {"Empty Bottle Belt", "Stone Bag"};
+RoninLoadouts["Sniper"]["Throwable"] = {"Empty Bottle Bandolier", "Stone Bag"};
 
 RoninLoadouts["Engineer"]["Headgear"] = {"Soldier Helmet"};
 RoninLoadouts["Engineer"]["Primary"] = {"UZI", "MP5K"};
 RoninLoadouts["Engineer"]["Secondary"] = {"Medium Digger", "Medium Digger", "Shovel"};
-RoninLoadouts["Engineer"]["Throwable"] = {"Remote Explosive", "Remote Explosive", "Scrambler Belt"};
+RoninLoadouts["Engineer"]["Throwable"] = {"Remote Explosive", "Remote Explosive", "Scrambler Bandolier"};
 
 RoninLoadouts["Medic"]["Headgear"] = {"Soldier Helmet"};
 RoninLoadouts["Medic"]["Primary"] = {"UZI", "MP5K"};
