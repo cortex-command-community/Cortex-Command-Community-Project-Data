767b8fe20c7321386aa48763fed0ead49d87337e
Gareth YR <garethrickwood@hotmail.com>
2023-01-04 03:14:43 -0400

    Trailing whitespace

--- Base.rte/Devices/Shared/Scripts/GrenadeBandolier.lua
+++ Base.rte/Devices/Shared/Scripts/GrenadeBandolier.lua
@@ -1,16 +1,16 @@
 function Create(self)
 	self.grenadeName = self:StringValueExists("GrenadeName") and self:GetStringValue("GrenadeName") or "Frag Grenade";
 	self.grenadeTech = self:StringValueExists("GrenadeTech") and self:GetStringValue("GrenadeTech") or "Base.rte";
-	
+
 	self.isThrownDevice = self:NumberValueExists("IsThrownDevice");
 	local appropriateGrenadeCreateFunction = self.isThrownDevice and CreateThrownDevice or CreateTDExplosive;
 	self.grenadeObject = appropriateGrenadeCreateFunction(self.grenadeName, self.grenadeTech);
 
 	self.bandolierKey =  self.grenadeTech .. "/" .. self.PresetName;
 	self.bandolierMass = self:NumberValueExists("BandolierMass") and self:GetNumberValue("BandolierMass") or 1.5;
-	
+
 	self.replenishDelay = self:NumberValueExists("ReplenishDelay") and self:GetNumberValue("ReplenishDelay") or 0;
-	
+
 	self.grenadeMass = self:NumberValueExists("GrenadeMass") and self:GetNumberValue("GrenadeMass") or self.grenadeObject.Mass;
 	self.grenadeCount = self:NumberValueExists("GrenadeCount") and self:GetNumberValue("GrenadeCount") or 3;
 
@@ -18,12 +18,12 @@ function Create(self)
 	-- Setup Grenade Bandolier Attachable --
 	----------------------------------------
 	self.attachable = CreateAttachable("Grenade Bandolier", "Base.rte");
-	
+
 	self.attachable:SetStringValue("BandolierName", self.PresetName);
 	self.attachable:SetNumberValue("BandolierMass", self.bandolierMass);
-	
+
 	self.attachable:SetNumberValue("ReplenishDelay", self.replenishDelay);
-	
+
 	self.attachable:SetStringValue("GrenadeName", self.grenadeName);
 	self.attachable:SetStringValue("GrenadeTech", self.grenadeTech);
 	if self.isThrownDevice then
