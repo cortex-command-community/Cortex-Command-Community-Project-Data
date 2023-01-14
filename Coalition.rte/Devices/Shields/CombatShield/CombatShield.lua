c8c6d11f8b16303741b1c8212915c94ffc25f88a
fourZK <the4zk@hotmail.com>
2023-01-14 22:51:44 +0200

    Combat Shield edits

--- Coalition.rte/Devices/Shields/CombatShield/CombatShield.lua
+++ Coalition.rte/Devices/Shields/CombatShield/CombatShield.lua
@@ -1,31 +1,31 @@
-function Create(self)
-	AttachCombatShield = function(self)
-		local errorSound = CreateSoundContainer("Error", "Base.rte");
-		local parent = ToAHuman(self:GetRootParent());
-		local device = parent:Inventory();
-		if device and device.ModuleName == self.ModuleName and IsHDFirearm(device) and not ToHDFirearm(device):IsOneHanded()then
-			device = ToHDFirearm(device);
-			for attachable in device.Attachables do
-				if attachable:GetModuleAndPresetName() == "Coalition.rte/Combat Shield" then
-					if parent:IsPlayerControlled() then
-						errorSound:Play(self.Pos);
-					end
-					return;
+function Coalition_AttachCombatShield(self)
+	local errorSound = CreateSoundContainer("Error", "Base.rte");
+	local parent = ToAHuman(self:GetRootParent());
+	local device = parent:Inventory();
+	if device and device.ModuleName == self.ModuleName and IsHDFirearm(device) and not ToHDFirearm(device):IsOneHanded() then
+		device = ToHDFirearm(device);
+		for attachable in device.Attachables do
+			if attachable:GetModuleAndPresetName() == "Coalition.rte/Combat Shield" then
+				if parent:IsPlayerControlled() then
+					errorSound:Play(self.Pos);
 				end
+				return;
 			end
-			self.Frame = 1;
-			device:AddAttachable(self:Clone(), Vector(math.floor(device.MuzzleOffset.X * 0.5) - 2, device.MuzzleOffset.Y));
-			if device:HasScript("Coalition.rte/Devices/Shields/CombatShield/Support.lua") then
-				device:EnableScript("Coalition.rte/Devices/Shields/CombatShield/Support.lua");
-			else
-				device:AddScript("Coalition.rte/Devices/Shields/CombatShield/Support.lua");
-			end
-			self.ToDelete = true;
-			parent:GetController():SetState(Controller.WEAPON_CHANGE_NEXT, true);
-		elseif parent:IsPlayerControlled() then
-			errorSound:Play(self.Pos);
 		end
+		self.Frame = 1;
+		device:AddAttachable(self:Clone(), Vector(math.floor(device.MuzzleOffset.X * 0.5) - 2, device.MuzzleOffset.Y));
+		if device:HasScript("Coalition.rte/Devices/Shields/CombatShield/Support.lua") then
+			device:EnableScript("Coalition.rte/Devices/Shields/CombatShield/Support.lua");
+		else
+			device:AddScript("Coalition.rte/Devices/Shields/CombatShield/Support.lua");
+		end
+		self.ToDelete = true;
+		parent:GetController():SetState(Controller.WEAPON_CHANGE_NEXT, true);
+	elseif parent:IsPlayerControlled() then
+		errorSound:Play(self.Pos);
 	end
+end
+function Create(self)
 	local parent = self:GetRootParent();
 	if IsAHuman(parent) then
 		parent = ToAHuman(parent);
@@ -33,14 +33,14 @@ function Create(self)
 		parent.PieMenu:GetFirstPieSliceByPresetName("Coalition Shield Detach PieSlice").Enabled = attachedToGun;
 		parent.PieMenu:GetFirstPieSliceByPresetName("Coalition Shield Attach PieSlice").Enabled = not attachedToGun;
 		if not attachedToGun and not parent:IsPlayerControlled() then
-			AttachCombatShield(self);
+			Coalition_AttachCombatShield(self);
 		end
 	end
 end
 function CoalitionShieldAttach(pieMenu, pieSlice, pieMenuOwner)
 	local device = pieMenuOwner.EquippedItem;
 	if device and IsHeldDevice(device) then
-		AttachCombatShield(ToHeldDevice(device));
+		Coalition_AttachCombatShield(ToHeldDevice(device));
 	end
 end
 function CoalitionShieldDetach(pieMenu, pieSlice, pieMenuOwner)
