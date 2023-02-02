43c819718bf53317444ebd6be5a56cd10b0ec9c4
getcetc <123813372+getcetc@users.noreply.github.com>
2023-02-02 20:52:09 +0700

    Update Hair.lua

--- Ronin.rte/Actors/Infantry/RoninLight/Hair.lua
+++ Ronin.rte/Actors/Infantry/RoninLight/Hair.lua
@@ -1,5 +1,5 @@
 function Update(self)
 	local pullForce = (self.Vel + self.PrevVel)/2 - SceneMan.GlobalAcc * rte.PxTravelledPerFrame;
 	self.RotAngle = pullForce.AbsRadAngle;
-	self.Frame = not pullForce:MagnitudeIsGreaterThan(8) and 1 or 0;
+	self.Frame = pullForce:MagnitudeIsLessThan(8) and 1 or 0;
 end
