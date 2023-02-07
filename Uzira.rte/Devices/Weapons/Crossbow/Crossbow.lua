e629c2449db97b53a0bc53aa54741819695a640e
fourZK <the4zk@hotmail.com>
2023-02-07 13:42:39 +0200

    More reload animations for Uzira Crossbow

--- Uzira.rte/Devices/Weapons/Crossbow/Crossbow.lua
+++ Uzira.rte/Devices/Weapons/Crossbow/Crossbow.lua
@@ -2,7 +2,17 @@ function Update(self)
 	if self.FiredFrame then
 		self.Frame = self.FrameCount - 1;
 	elseif self:IsReloading() then
-		self.Frame = math.min(math.ceil((self.FrameCount) * (1 - self.ReloadProgress)), self.Frame);
+		local inverseProgress = 1 - self.ReloadProgress;
+		self.Frame = math.min(math.ceil((self.FrameCount) * inverseProgress), self.Frame);
+		local parent = self:GetRootParent();
+		if IsAHuman(parent) then
+			parent = ToAHuman(parent);
+			if parent.FGArm and parent.BGArm then
+				pullArm = self:GetParent().ID == parent.BGArm.ID and parent.FGArm or parent.BGArm;
+				pullArm:ClearHandTargets();
+				pullArm.HandPos = self.Pos + Vector(6 * self.FlipFactor * math.sin(inverseProgress * math.pi), -2):RadRotate(self.RotAngle);
+			end
+		end
 	elseif self:DoneReloading() then
 		self.Frame = 0;
 	end
