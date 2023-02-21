2ffb67169d47925d9500d6661012d3f055c1b650
fourZK <the4zk@hotmail.com>
2023-02-21 11:12:27 +0200

    Air blast script should only affect hittable MOs

--- Base.rte/Scripts/AirBlast.lua
+++ Base.rte/Scripts/AirBlast.lua
@@ -6,7 +6,7 @@ end
 function Update(self)
 	--Run the effect on Update() to give other particles a chance to reach the target
 	for mo in MovableMan:GetMOsInRadius(self.Pos, self.range) do
-		if IsMOSRotating(mo) and mo.PinStrength == 0 then
+		if mo.GetsHitByMOs and mo.PinStrength == 0 and IsMOSRotating(mo) then
 			local dist = SceneMan:ShortestDistance(self.Pos, mo.Pos, SceneMan.SceneWrapsX);
 			local strSumCheck = SceneMan:CastStrengthSumRay(self.Pos, self.Pos + dist, 3, rte.airID);
 			if strSumCheck < self.strength then
