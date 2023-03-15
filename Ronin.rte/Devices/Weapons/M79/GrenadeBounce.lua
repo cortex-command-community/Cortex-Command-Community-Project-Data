c4fc659baa588268cc5d8d8e1024ecd5ea321561
fourZK <the4zk@hotmail.com>
2023-03-15 19:22:24 +0200

    Fix M79 safety thing with newly added `DistanceTravelled` property

--- Ronin.rte/Devices/Weapons/M79/GrenadeBounce.lua
+++ Ronin.rte/Devices/Weapons/M79/GrenadeBounce.lua
@@ -1,20 +1,12 @@
 function Create(self)
-	self.detonationDelay = 3000;
-	self.startPos = Vector(self.Pos.X, self.Pos.Y);
 	self.safetyRadius = 180;
 end
-
 function Update(self)
-	if not self.safetyTriggered then
-		if self.Age > self.detonationDelay or self.TravelImpulse:MagnitudeIsGreaterThan(self.Mass) then
-			if SceneMan:ShortestDistance(self.startPos, self.Pos, SceneMan.SceneWrapsX):MagnitudeIsGreaterThan(self.safetyRadius) then
-				self:GibThis();
-			else
-				self.safetyTriggered = true;
-			end
+	if self.TravelImpulse:MagnitudeIsGreaterThan(self.Mass) then
+		if self.DistanceTravelled > self.safetyRadius then
+			self:GibThis();
 		else
-			self.ToDelete = false;
-			self.ToSettle = false;
+			self:DisableScript("Ronin.rte/Devices/Weapons/M79/GrenadeBounce.lua");
 		end
 	end
 end
--- Ronin.rte/Devices/Weapons/M79/M79Pie.lua
+++ Ronin.rte/Devices/Weapons/M79/M79Pie.lua
@@ -4,8 +4,10 @@ function M79GrenadeLauncherFailsafeOff(pieMenu, pieSlice, pieMenuOwner)
 		local gun = ToHDFirearm(gun);
 		local magSwitchName = "Magazine Ronin M79 Grenade Launcher Impact";
 		if gun.Magazine == nil or gun.Magazine.PresetName ~= magSwitchName then
+			if gun.Magazine then
+				gun.Magazine = CreateMagazine(magSwitchName, "Ronin.rte");
+			end
 			gun:SetNextMagazineName(magSwitchName);
-			gun:Reload();
 			pieMenu:ReplacePieSlice(pieSlice, CreatePieSlice("M79 Turn On Failsafe", "Ronin.rte"));
 		end
 	end
@@ -17,8 +19,10 @@ function M79GrenadeLauncherFailsafeOn(pieMenu, pieSlice, pieMenuOwner)
 		local gun = ToHDFirearm(gun);
 		local magSwitchName = "Magazine Ronin M79 Grenade Launcher Bounce";
 		if gun.Magazine == nil or gun.Magazine.PresetName ~= magSwitchName then
+			if gun.Magazine then
+				gun.Magazine = CreateMagazine(magSwitchName, "Ronin.rte");
+			end
 			gun:SetNextMagazineName(magSwitchName);
-			gun:Reload();
 			pieMenu:ReplacePieSlice(pieSlice, CreatePieSlice("M79 Turn Off Failsafe", "Ronin.rte"));
 		end
 	end
