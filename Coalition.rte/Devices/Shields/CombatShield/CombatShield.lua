5f5f4bf2436c0575f66e6d07a5a1e89bc71b0b70
Gareth YR <garethrickwood@hotmail.com>
2023-05-17 13:03:58 -0300

    Fixing up combat shield attach script so it's less fragile and not broken

--- Coalition.rte/Devices/Shields/CombatShield/CombatShield.lua
+++ Coalition.rte/Devices/Shields/CombatShield/CombatShield.lua
@@ -1,28 +1,34 @@
 function Coalition_AttachCombatShield(self)
-	local errorSound = CreateSoundContainer("Error", "Base.rte");
 	local parent = ToAHuman(self:GetRootParent());
-	local device = parent:Inventory();
-	if device and device.ModuleName == self.ModuleName and IsHDFirearm(device) and not ToHDFirearm(device):IsOneHanded() then
-		device = ToHDFirearm(device);
-		for attachable in device.Attachables do
-			if attachable:GetModuleAndPresetName() == "Coalition.rte/Combat Shield" then
-				if parent:IsPlayerControlled() then
-					errorSound:Play(self.Pos, parent:GetController().Player);
+	
+	for device in parent.Inventory do
+		if device.ModuleName == self.ModuleName and IsHDFirearm(device) and not ToHDFirearm(device):IsOneHanded() then
+			device = ToHDFirearm(device);
+			
+			local canAttachCombatShield = true;
+			for attachable in device.Attachables do
+				if attachable:GetModuleAndPresetName() == "Coalition.rte/Combat Shield" then
+					canAttachCombatShield = false;
 				end
+			end
+			
+			if canAttachCombatShield then
+				self.Frame = 1;
+				parent.PieMenu:RemovePieSlicesByOriginalSource(self);
+				device:AddAttachable(self:Clone(), Vector(math.floor(device.MuzzleOffset.X * 0.5) - 2, device.MuzzleOffset.Y));
+				if device:HasScript("Coalition.rte/Devices/Shields/CombatShield/Support.lua") then
+					device:EnableScript("Coalition.rte/Devices/Shields/CombatShield/Support.lua");
+				else
+					device:AddScript("Coalition.rte/Devices/Shields/CombatShield/Support.lua");
+				end
+				self.ToDelete = true;
+				parent:EquipNamedDevice(device.ModuleName, device.PresetName, true);
 				return;
 			end
 		end
-		self.Frame = 1;
-		device:AddAttachable(self:Clone(), Vector(math.floor(device.MuzzleOffset.X * 0.5) - 2, device.MuzzleOffset.Y));
-		if device:HasScript("Coalition.rte/Devices/Shields/CombatShield/Support.lua") then
-			device:EnableScript("Coalition.rte/Devices/Shields/CombatShield/Support.lua");
-		else
-			device:AddScript("Coalition.rte/Devices/Shields/CombatShield/Support.lua");
-		end
-		self.ToDelete = true;
-		parent:GetController():SetState(Controller.WEAPON_CHANGE_NEXT, true);
-	elseif parent:IsPlayerControlled() then
-		errorSound:Play(self.Pos, parent:GetController().Player);
+	end
+	if parent:IsPlayerControlled() then
+		CreateSoundContainer("Error", "Base.rte"):Play(self.Pos, parent:GetController().Player);
 	end
 end
 
@@ -39,9 +45,25 @@ function Create(self)
 	end
 end
 
+function OnAttach(self, newParent)
+	local parent = self:GetRootParent();
+	if IsAHuman(parent) then
+		parent = ToAHuman(parent);
+		local attachedToGun = IsHDFirearm(self:GetParent());
+		parent.PieMenu:GetFirstPieSliceByPresetName("Coalition Shield Detach PieSlice").Enabled = attachedToGun;
+		parent.PieMenu:GetFirstPieSliceByPresetName("Coalition Shield Attach PieSlice").Enabled = not attachedToGun;
+		if not attachedToGun and not parent:IsPlayerControlled() then
+			Coalition_AttachCombatShield(self);
+		end
+	end
+end
+
 function CoalitionShieldAttach(pieMenuOwner, pieMenu, pieSlice)
 	local device = pieMenuOwner.EquippedItem;
-	if device and IsHeldDevice(device) then
+	if device:GetModuleAndPresetName() ~= "Coalition.rte/Combat Shield" then
+		device = pieMenuOwner.EquippedBGItem;
+	end
+	if device and IsHeldDevice(device) and device:GetModuleAndPresetName() == "Coalition.rte/Combat Shield" then
 		Coalition_AttachCombatShield(ToHeldDevice(device));
 	end
 end
@@ -58,7 +80,8 @@ function CoalitionShieldDetach(pieMenuOwner, pieMenu, pieSlice)
 					device:DisableScript("Coalition.rte/Devices/Shields/CombatShield/Support.lua");
 				end
 				attachable.Frame = 0;
-				device:RemoveAttachable(attachable, true, true);
+				local removedAttachable = device:RemoveAttachable(attachable, false, false);
+				pieMenuOwner:AddInventoryItem(removedAttachable:Clone())
 				break;
 			end
 		end
