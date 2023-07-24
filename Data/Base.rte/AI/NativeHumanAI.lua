fac05685cdfd79f8d35219cb787e9138826eb2fb
Causeless <tommymarch5@gmail.com>
2023-07-24 14:03:21 +0100

    Increase fuel instead of thrust for AI

--- Data/Base.rte/AI/NativeHumanAI.lua
+++ Data/Base.rte/AI/NativeHumanAI.lua
@@ -53,11 +53,9 @@ function NativeHumanAI:Create(Owner)
 	-- the native AI assume the jetpack cannot be destroyed
 	if Owner.Jetpack then
 		if not Members.isPlayerOwned then
-			Owner.Jetpack.Throttle = Owner.Jetpack.Throttle + 0.15	-- increase jetpack strength slightly to compensate for AI ineptitude
+			Owner.Jetpack.JetTimeTotal = Owner.Jetpack.JetTimeTotal * 1.2;	-- increase jetpack fuel to compensate for extra fuel spend
 		end
 
-		Members.jetImpulseFactor = Owner.Jetpack:EstimateImpulse(false) * GetPPM() / TimerMan.DeltaTimeSecs;
-		Members.jetBurstFactor = (Owner.Jetpack:EstimateImpulse(true) * GetPPM() / TimerMan.DeltaTimeSecs - Members.jetImpulseFactor) * math.pow(TimerMan.DeltaTimeSecs, 2) * 0.5;
 		Members.minBurstTime = math.min(Owner.Jetpack.BurstSpacing*2, Owner.Jetpack.JetTimeTotal*0.99); -- in milliseconds
 	end
 
@@ -68,6 +66,12 @@ end
 
 function NativeHumanAI:Update(Owner)
 	self.Ctrl = Owner:GetController();
+
+	-- Our jetpack might have thrust balancing enabled, so update for our current mass
+	if Owner.Jetpack then		
+		Members.jetImpulseFactor = Owner.Jetpack:EstimateImpulse(false) * GetPPM() / TimerMan.DeltaTimeSecs;
+		Members.jetBurstFactor = (Owner.Jetpack:EstimateImpulse(true) * GetPPM() / TimerMan.DeltaTimeSecs - Members.jetImpulseFactor) * math.pow(TimerMan.DeltaTimeSecs, 2) * 0.5;
+	end
 
 	if self.isPlayerOwned then
 		if self.PlayerInterferedTimer:IsPastSimTimeLimit() then
