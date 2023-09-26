026ef4a5ce6584b169c695f66ef95150465ba2a9
pawnishoovy <54544349+pawnishoovy@users.noreply.github.com>
2023-09-26 22:11:13 +0300

    jumppack, shockwave ability

--- Data/Browncoats.rte/Actors/Infantry/BrowncoatBoss/AI/BrowncoatBossAI.lua
+++ Data/Browncoats.rte/Actors/Infantry/BrowncoatBoss/AI/BrowncoatBossAI.lua
@@ -16,6 +16,16 @@ function Create(self)
 	mainWeapon:SetNumberValue("Boss Mode", 1);
 	self:AddInventoryItem(mainWeapon);
 	
+	self.abilityShockwaveWhooshSound = CreateSoundContainer("Browncoat Boss Ability Shockwave Whoosh", "Browncoats.rte");	
+	self.abilityShockwaveLandSound = CreateSoundContainer("Browncoat Boss Ability Shockwave Land", "Browncoats.rte");	
+	
+	self.abilityShockwaveTimer = Timer();
+	self.abilityShockwaveJumpPackDelay = 300;
+	
+	self.abilityShockwaveScreenShakeAmount = 30;
+	self.abilityShockwaveRange = 1500;
+	self.abilityShockwaveStrength = 700;
+	
 	self.quickThrowTimer = Timer();
 	self.quickThrowDelay = 15000;
 	self.quickThrowExplosive = CreateTDExplosive("Fuel Bomb", "Browncoats.rte");	
@@ -24,13 +34,19 @@ end
 
 function Update(self)
 
-	if not self:HasObjectInGroup("Bombs") then 
-	
-		local explosive = self.quickThrowExplosive:Clone();
-		explosive.MinThrowVel = 30;
-		explosive.MaxThrowVel = 30;
-		self:AddInventoryItem(explosive);
-				
+	self.abilityShockwaveWhooshSound.Pos = self.Pos;
+
+	if not self:IsDead() then
+
+		if not self:HasObjectInGroup("Bombs") then 
+		
+			local explosive = self.quickThrowExplosive:Clone();
+			explosive.MinThrowVel = 30;
+			explosive.MaxThrowVel = 30;
+			self:AddInventoryItem(explosive);
+					
+		end
+		
 	end
 
 	if not self:IsPlayerControlled() then -- just in case
@@ -43,6 +59,28 @@ function Update(self)
 
 				self.AI:CreateQuickthrowBehavior(self);
 				
+			end
+		end
+	end
+	
+	-- DEBUG SHOCKWAVE ABILITY
+	
+	local debugTrigger = UInputMan:KeyPressed(Key.O);
+	
+	if debugTrigger then
+		self.abilityShockwaveTrigger = true;
+	end
+	
+	if self.abilityShockwaveOngoing then
+		if self.abilityShockwaveTimer:IsPastSimMS(self.abilityShockwaveJumpPackDelay) then
+		
+			self.Jetpack.NegativeThrottleMultiplier = self.jumpPackDefaultNegativeMult * 1.3;
+			self.Jetpack.PositiveThrottleMultiplier = self.jumpPackDefaultPositiveMult * 1.3;
+			
+			self.controller:SetState(Controller.BODY_JUMPSTART, true)
+			self.controller:SetState(Controller.BODY_JUMP, true)
+			if not self.abilityShockwaveWhooshSound:IsBeingPlayed() then
+				self.abilityShockwaveWhooshSound:Play(self.Pos);
 			end
 		end
 	end
--- Data/Browncoats.rte/Actors/Infantry/BrowncoatBoss/BrowncoatBoss.lua
+++ Data/Browncoats.rte/Actors/Infantry/BrowncoatBoss/BrowncoatBoss.lua
@@ -1,6 +1,154 @@
 dofile("Base.rte/Constants.lua")
 
+BrowncoatBossFunctions = {};
+
+function BrowncoatBossFunctions.createVoiceSoundEffect(self, soundContainer, priority, canOverridePriority)
+	if canOverridePriority == nil then
+		canOverridePriority = false;
+	end
+	local usingPriority
+	if canOverridePriority == false then
+		usingPriority = priority - 1;
+	else
+		usingPriority = priority;
+	end
+	if self.Head and soundContainer ~= nil then
+		if self.voiceSound then
+			if self.voiceSound:IsBeingPlayed() then
+				if self.lastPriority <= usingPriority then
+					self.voiceSound:Stop();
+					self.voiceSound = soundContainer;
+					soundContainer:Play(self.Pos)
+					self.lastPriority = priority;
+					return true;
+				end
+			else
+				self.voiceSound = soundContainer;
+				soundContainer:Play(self.Pos)
+				self.lastPriority = priority;
+				return true;
+			end
+		else
+			self.voiceSound = soundContainer;
+			soundContainer:Play(self.Pos)
+			self.lastPriority = priority;
+			return true;
+		end
+	end
+end
+
+function BrowncoatBossFunctions.abilityShockwaveLanding(self)
+
+	self.Jetpack.NegativeThrottleMultiplier = self.jumpPackDefaultNegativeMult;
+	self.Jetpack.PositiveThrottleMultiplier = self.jumpPackDefaultPositiveMult;
+
+	CameraMan:AddScreenShake(self.abilityShockwaveScreenShakeAmount, self.Pos);
+	
+	-- Breakable Ground Smoke
+	for i = 1, 15 do	
+		local effect = CreateMOSRotating("Breakable Smoke Ball Tiny", "Base.rte")
+		effect.Pos = self.Pos;
+		effect.Vel = self.Vel + Vector(math.random(-50, 50),math.random(90,150))
+		effect.Lifetime = effect.Lifetime * RangeRand(0.5,2.0)
+		effect.AirResistance = effect.AirResistance * RangeRand(0.5,0.8)
+		MovableMan:AddParticle(effect)
+	end
+	
+	for i = 1, 10 do	
+		local effect = CreateMOSRotating("Breakable Smoke Ball", "Base.rte")
+		effect.Pos = self.Pos;
+		effect.Vel = self.Vel + Vector(math.random(-50, 50),math.random(90,150))
+		effect.Lifetime = effect.Lifetime * RangeRand(0.5,2.0)
+		effect.AirResistance = effect.AirResistance * RangeRand(0.5,0.8)
+		MovableMan:AddParticle(effect)
+	end
+
+	-- Funkier Ground Smoke
+	
+	local groundPos = SceneMan:MovePointToGround(self.Pos, 1, 1);
+	
+	for i = 1, 25 do	
+		local effect = CreateMOSParticle("Smoke Ball 1", "Base.rte")
+		effect.Pos = groundPos;
+		effect.Vel = self.Vel + Vector(math.random(-50, 50),math.random(-20,-30))
+		effect.Lifetime = effect.Lifetime * RangeRand(0.5,2.0)
+		effect.AirResistance = effect.AirResistance * RangeRand(0.5,0.8)
+		MovableMan:AddParticle(effect)
+	end
+	
+	for i = 1, 25 do	
+		local effect = CreateMOSParticle("Tiny Smoke Ball 1", "Base.rte")
+		effect.Pos = groundPos;
+		effect.Vel = self.Vel + Vector(math.random(-50, 50),math.random(-20,-30))
+		effect.Lifetime = effect.Lifetime * RangeRand(0.5,2.0)
+		effect.AirResistance = effect.AirResistance * RangeRand(0.5,0.8)
+		MovableMan:AddParticle(effect)
+	end
+	
+
+	for mo in MovableMan:GetMOsInRadius(self.Pos, self.abilityShockwaveRange, -1, true) do
+		if mo.Team ~= self.Team and mo.PinStrength == 0 and IsMOSRotating(mo) then
+			local dist = SceneMan:ShortestDistance(self.Pos, mo.Pos, SceneMan.SceneWrapsX);
+			local strSumCheck = SceneMan:CastStrengthSumRay(self.Pos, self.Pos + dist, 3, rte.airID);
+			if strSumCheck < self.abilityShockwaveStrength then
+				local massFactor = math.sqrt(1 + math.abs(mo.Mass));
+				local distFactor = 1 + dist.Magnitude * 0.1;
+				local forceVector =	dist:SetMagnitude((self.abilityShockwaveStrength - strSumCheck)/distFactor);
+				if IsAttachable(mo) then
+					--Diminish transferred impulses from attachables since we are likely already targeting its' parent
+					forceVector = forceVector * math.abs(1 - ToAttachable(mo).JointStiffness);
+				end
+				mo.Vel = mo.Vel + forceVector/massFactor;
+				mo.AngularVel = mo.AngularVel - forceVector.X/(massFactor + math.abs(mo.AngularVel));
+				mo:AddImpulseForce(forceVector * massFactor, Vector());
+				--Add some additional points of damage to actors
+				if IsActor(mo) then
+					local actor = ToActor(mo);
+					local impulse = (forceVector.Magnitude * self.abilityShockwaveStrength/massFactor) - actor.ImpulseDamageThreshold;
+					local damage = impulse/(actor.GibImpulseLimit * 0.1 + actor.Material.StructuralIntegrity * 10);
+					actor.Health = damage > 0 and actor.Health - damage or actor.Health;
+					actor.Status = (actor.Status == Actor.STABLE and damage > (actor.Health * 0.7)) and Actor.UNSTABLE or actor.Status;
+				end
+			end
+		end
+	end
+
+end
+
+function BrowncoatBossFunctions.JumpPack(self)
+
+	self.jetpackEmitting = true;
+
+	self.jumpPackCooldownTimer:Reset();
+	
+	self.isInAir = true;
+	--self.jumpPackSound:Play(self.Pos);
+	
+	local offset = Vector(0, 2)
+	
+	local emitterA = CreateAEmitter("Browncoat Boss JumpPack Smoke Trail Medium")
+	emitterA.Lifetime = 1300
+	self.Jetpack:AddAttachable(emitterA);
+	
+	ToAttachable(emitterA).ParentOffset = offset
+	
+	local emitterB = CreateAEmitter("Browncoat Boss JumpPack Smoke Trail Heavy")
+	emitterB.Lifetime = 400
+	self.Jetpack:AddAttachable(emitterB);
+	
+	ToAttachable(emitterB).ParentOffset = offset
+	
+end
+
 function Create(self)
+
+	self.voiceSounds = {
+	JumpAttack = CreateSoundContainer("Browncoat Boss VO JumpAttack", "Browncoats.rte")}
+	
+	self.voiceSound = CreateSoundContainer("Browncoat Boss JumpPack", "Browncoats.rte");
+	-- MEANINGLESS! this is just so we can do voiceSound.Pos without an if check first! it will be overwritten first actual VO play
+
+	self.jumpPackSound = CreateSoundContainer("Browncoat Boss JumpPack", "Browncoats.rte");
 
 	self.stepSound = CreateSoundContainer("Browncoat Boss Stride", "Browncoats.rte");	
 	self.jumpSound = CreateSoundContainer("Browncoat Boss Jump", "Browncoats.rte");	
@@ -12,13 +160,28 @@ function Create(self)
     self.feetTimers = {Timer(), Timer()}
 	self.footstepTime = 100 -- 2 Timers to avoid noise	
 	
-	
+	-- Custom Jumping
+	self.jumpStrength = 4
+	self.isJumping = false
+	self.jumpTimer = Timer();
+	self.jumpDelay = 500;
 	self.jumpStop = Timer();
 	
+
+	self.jumpPackDefaultNegativeMult = 8;
+	self.jumpPackDefaultPositiveMult = 12;
+	self.jumpPackCooldownTimer = Timer();
+	self.jumpPackCooldownTime = 0;
+	
 	self.altitude = 0;
-	self.wasInAir = false;
+	self.isInAir = false;
 	
 	self.moveSoundTimer = Timer();
+	
+	
+	if self.PresetName == "Browncoat Boss Scripted" then
+		self.bossMode = true;
+	end
 	
 end
 
@@ -67,15 +230,17 @@ end
 
 function Update(self)
 
+	self.voiceSound.Pos = self.Pos;
+
 	self.controller = self:GetController();
 
 	-- Leg Collision Detection system
     --local i = 0
 	if self:IsPlayerControlled() then -- AI doesn't update its own foot checking when playercontrolled so we have to do it
-		if self.Vel.Y > 10 then
-			self.wasInAir = true;
+		if self.Vel.Y > 5 then
+			self.isInAir = true;
 		else
-			self.wasInAir = false;
+			self.isInAir = false;
 		end
 		for i = 1, 2 do
 			--local foot = self.feet[i]
@@ -121,25 +286,85 @@ function Update(self)
 			end
 		end
 	else
-		if self.AI.flying == true and self.wasInAir == false then
-			self.wasInAir = true;
-		elseif self.AI.flying == false and self.wasInAir == true then
-			self.wasInAir = false;
-			if self.moveSoundTimer:IsPastSimMS(500) then
-				self.landSound:Play(self.Pos);
-				self.stepSound:Play(self.Pos);
-				self.moveSoundTimer:Reset();
-			end
+		if self.AI.flying == true and self.isInAir == false then
+			self.isInAir = true;
+		elseif self.AI.flying == false and self.isInAir == true then
+			self.isInAir = false;
 		end
 	end
 	
-	if self.wasInAir then
+	-- Jumppack custom fx, for extra control actor-side
+	
+	local jumpPackTrigger = self.Jetpack:IsEmitting() and not self.jetpackEmitting;
+	
+	if jumpPackTrigger then
+		BrowncoatBossFunctions.JumpPack(self)
+	end
+	
+	if not self.Jetpack:IsEmitting() then
+		self.jetpackEmitting = false;
+	end
+
+	-- Jump to make this guy more bearable movement-wise
+	-- Also for extra cool boss ability
+	
+	local jump = (self.controller:IsState(Controller.BODY_JUMPSTART) == true and
+		self.controller:IsState(Controller.BODY_CROUCH) == false and
+		self.jumpTimer:IsPastSimMS(self.jumpDelay) and
+		not self.isJumping and
+		not jumpPackTrigger)
+	
+	if jump or self.abilityShockwaveTrigger then
+		if (self:IsPlayerControlled() and self.feetContact[1] == true or self.feetContact[2] == true) or self.isInAir == false then
+			local jumpVec = Vector(0,-self.jumpStrength)
+			
+			if self.abilityShockwaveTrigger then
+			
+				jumpVec = jumpVec * 2
+			
+				self.abilityShockwaveTrigger = false;
+				self.abilityShockwaveTimer:Reset();
+				self.abilityShockwaveOngoing = true;
+				BrowncoatBossFunctions.createVoiceSoundEffect(self, self.voiceSounds.JumpAttack, 10, true);
+			end			
+			
+			local jumpWalkX = 3
+			if self.controller:IsState(Controller.MOVE_LEFT) == true then
+				jumpVec.X = -jumpWalkX
+			elseif self.controller:IsState(Controller.MOVE_RIGHT) == true then
+				jumpVec.X = jumpWalkX
+			end
+			self.jumpSound:Play(self.Pos);
+			if math.abs(self.Vel.X) > jumpWalkX * 2.0 then
+				self.Vel = Vector(self.Vel.X, self.Vel.Y + jumpVec.Y)
+			else
+				self.Vel = Vector(self.Vel.X + jumpVec.X, self.Vel.Y + jumpVec.Y)
+			end
+			self.isJumping = true
+			self.jumpTimer:Reset()
+			self.jumpStop:Reset()
+		end
+		
+	elseif self.isJumping or self.isInAir then
 		if (self:IsPlayerControlled() and self.feetContact[1] == true or self.feetContact[2] == true) and self.jumpStop:IsPastSimMS(100) then
-			self.wasInAir = false;
-			if self.Vel.Y > 0 and self.moveSoundTimer:IsPastSimMS(500) then
-				self.landSound:Play(self.Pos);
-				self.stepSound:Play(self.Pos);
-				self.moveSoundTimer:Reset();			
+			self.isJumping = false
+			self.isInAir = false;
+			if (self.Vel.Y > 0 and self.moveSoundTimer:IsPastSimMS(500)) or self.abilityShockwaveOngoing == true then
+			
+				if self.abilityShockwaveOngoing == true then
+					self.abilityShockwaveOngoing = false;
+					self.abilityShockwaveWhooshSound:Stop(-1);
+					self.voiceSound:FadeOut(90);
+					
+					self.abilityShockwaveLandSound:Play(self.Pos);
+					
+					BrowncoatBossFunctions.abilityShockwaveLanding(self);
+				else
+					self.landSound:Play(self.Pos);
+				end
+				
+				self.moveSoundTimer:Reset();
+				
 				
 			end
 		end
