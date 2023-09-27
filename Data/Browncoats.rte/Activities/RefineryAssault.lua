4753bb4cca91fb9c01bfa90e2ee8c04bdcafd426
pawnishoovy <54544349+pawnishoovy@users.noreply.github.com>
2023-09-27 12:48:27 +0300

    dummy terrain scene added

---
+++ Data/Browncoats.rte/Activities/RefineryAssault.lua
@@ -0,0 +1,85 @@
+package.loaded.Constants = nil; require("Constants");
+
+-----------------------------------------------------------------------------------------
+-- Start Activity
+-----------------------------------------------------------------------------------------
+
+function RefineryAssault:StartActivity()
+	print("START! -- Test:StartActivity()!");
+
+	for player = Activity.PLAYER_1, Activity.MAXPLAYERCOUNT - 1 do
+		if self:PlayerActive(player) and self:PlayerHuman(player) then
+			-- Check if we already have a brain assigned
+			if not self:GetPlayerBrain(player) then
+				local foundBrain = MovableMan:GetUnassignedBrain(self:GetTeamOfPlayer(player));
+				-- If we can't find an unassigned brain in the scene to give each player, then force to go into editing mode to place one
+				if not foundBrain then
+					self.ActivityState = Activity.EDITING;
+					-- Open all doors so we can do pathfinding through them with the brain placement
+					MovableMan:OpenAllDoors(true, Activity.NOTEAM);
+					AudioMan:ClearMusicQueue();
+					AudioMan:PlayMusic("Base.rte/Music/dBSoundworks/ccambient4.ogg", -1, -1);
+					self:SetLandingZone(Vector(player*SceneMan.SceneWidth/4, 0), player);
+				else
+					-- Set the found brain to be the selected actor at start
+					self:SetPlayerBrain(foundBrain, player);
+					self:SwitchToActor(foundBrain, player, self:GetTeamOfPlayer(player));
+					self:SetLandingZone(self:GetPlayerBrain(player).Pos, player);
+					-- Set the observation target to the brain, so that if/when it dies, the view flies to it in observation mode
+					self:SetObservationTarget(self:GetPlayerBrain(player).Pos, player);
+				end
+			end
+		end
+	end
+
+	self.doorMessageTimer = Timer();
+	self.doorMessageTimer:SetSimTimeLimitMS(5000);
+	self.allDoorsOpened = false;
+end
+
+function RefineryAssault:OnSave()
+	-- Don't have to do anything, just need this to allow saving/loading.
+end
+
+-----------------------------------------------------------------------------------------
+-- Pause Activity
+-----------------------------------------------------------------------------------------
+
+function RefineryAssault:PauseActivity(pause)
+	print("PAUSE! -- Test:PauseActivity()!");
+end
+
+-----------------------------------------------------------------------------------------
+-- End Activity
+-----------------------------------------------------------------------------------------
+
+function RefineryAssault:EndActivity()
+	print("END! -- Test:EndActivity()!");
+end
+
+-----------------------------------------------------------------------------------------
+-- Update Activity
+-----------------------------------------------------------------------------------------
+
+function RefineryAssault:UpdateActivity()
+	if self.doorMessageTimer then
+		for player = Activity.PLAYER_1, Activity.MAXPLAYERCOUNT - 1 do
+			if self:PlayerActive(player) and self:PlayerHuman(player) then
+				FrameMan:SetScreenText("NOTE: You can press ALT + 1 to open or close all doors", player, 0, -1, false);
+			end
+		end
+		if self.doorMessageTimer:IsPastSimTimeLimit() then
+			self.doorMessageTimer = nil;
+			for player = Activity.PLAYER_1, Activity.MAXPLAYERCOUNT - 1 do
+				if self:PlayerActive(player) and self:PlayerHuman(player) then
+					FrameMan:ClearScreenText(player);
+				end
+			end
+		end
+	end
+
+	if UInputMan.FlagAltState and UInputMan:KeyPressed(Key.K_1) then
+		MovableMan:OpenAllDoors(not self.allDoorsOpened, Activity.NOTEAM);
+		self.allDoorsOpened = not self.allDoorsOpened;
+	end
+end
