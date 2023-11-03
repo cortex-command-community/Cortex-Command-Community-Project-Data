744323e5fa27cdd2320e68e2e5c15bb307e9fd6b
pawnishoovy <54544349+pawnishoovy@users.noreply.github.com>
2023-11-03 11:54:59 +0200

    better capturable sounds

--- Data/Base.rte/Scenes/Objects/Bunkers/BunkerSystems/GenericCapturable/GenericCapturableFX.lua
+++ Data/Base.rte/Scenes/Objects/Bunkers/BunkerSystems/GenericCapturable/GenericCapturableFX.lua
@@ -2,8 +2,12 @@ function Create(self)
 	
 	self.startCaptureSound = CreateSoundContainer("Capturable Start Capture", "Base.rte");
 	self.capturingSound = CreateSoundContainer("Capturable Capturing", "Base.rte");
+	self.capturingCriticalSound = CreateSoundContainer("Capturable Capturing Critical", "Base.rte");
 	self.stopCaptureSound = CreateSoundContainer("Capturable Stop Capture", "Base.rte");
 	self.captureSuccessSound = CreateSoundContainer("Capturable Capture Success", "Base.rte");
+	
+	self.capturingTimer = Timer();
+	self.capturingDelay = 500;
 	
 end
 
@@ -11,9 +15,18 @@ function Update(self)
 
 	if self.FXstartCapture then
 		self.startCaptureSound:Play(self.Pos);
+		self.capturingTimer:Reset();
 	end
-	if self.FXcapturing and not self.capturingSound:IsBeingPlayed() then
-		self.capturingSound:Play(self.Pos);
+	if self.FXcapturing then
+		local sound = self.capturingSound;
+		self.capturingDelay = 500;
+		if self.dominantTeam == self.capturingTeam then
+			self.capturingDelay = 250;
+		end
+		if self.capturingTimer:IsPastSimMS(self.capturingDelay) then
+			self.capturingTimer:Reset();
+			sound:Play(self.Pos);
+		end
 	end
 	if self.FXstopCapture then
 		self.stopCaptureSound:Play(self.Pos);
