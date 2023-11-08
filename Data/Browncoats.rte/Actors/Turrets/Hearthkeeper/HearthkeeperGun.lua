5888ba22959efc866f5e8285b5bda37d05b6576b
pawnishoovy <54544349+pawnishoovy@users.noreply.github.com>
2023-11-09 00:58:46 +0200

    minigun turret aligning, sounds
    ceiling misaligns itself, dunno why

--- Data/Browncoats.rte/Actors/Turrets/Hearthkeeper/HearthkeeperGun.lua
+++ Data/Browncoats.rte/Actors/Turrets/Hearthkeeper/HearthkeeperGun.lua
@@ -1,5 +1,10 @@
 function OnFire(self)
 	
+	if not self.Firing then
+		self.Firing = true;
+		self.startShotSound:Play(self.Pos);
+	end
+	
 end
 
 function OnReload(self)
@@ -8,14 +13,49 @@ end
 
 function Create(self)
 
+	self.spinUpSound = CreateSoundContainer("Spin Up Browncoat RG-1400", "Browncoat.rte");
+	self.spinDownSound = CreateSoundContainer("Spin Down Browncoat RG-1400", "Browncoat.rte");
+	self.startShotSound = CreateSoundContainer("Start Shot Browncoat RG-1400", "Browncoat.rte");
+	self.tailSound = CreateSoundContainer("Tail Browncoat RG-1400", "Browncoat.rte");
+	
+	self.spinUpTimer = Timer();
+	self.spinUpTime = 1000;
 
 end
 
 function Update(self)
 
+	local fire = self:IsActivated() and self.RoundInMagCount > 0;
+	
+	if fire then
+		if not self.spinUpTimer:IsPastSimMS(self.spinUpTime) then
+			if not self.spinningUp then
+				self.spinningUp = true;
+				self.spinUpSound:Play(self.Pos);
+			end
+			self:Deactivate();
+		end
+			
+	else
+		if self.Firing then
+			self.spinningUp = false;
+			self.Firing = false;
+			self.tailSound:Play(self.Pos);
+			self.spinDownSound:Play(self.Pos);
+		end
+		if self.spinningUp then
+			self.spinningUp = false;
+			self.spinDownSound:Play(self.Pos);
+			self.spinUpSound:FadeOut(100);
+		end
+		self.spinUpTimer:Reset();
+	end
+
 end
 
 function Destroy(self)
 
+	self.spinUpSound:Stop(-1);
+	self.spinDownSound:Stop(-1);
 	
 end
