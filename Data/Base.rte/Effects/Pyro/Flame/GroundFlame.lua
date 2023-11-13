db1cb361fc98e6ccb11dc1352acb6638f6664f0b
Causeless <tommymarch5@gmail.com>
2023-11-13 18:32:35 +0000

    Multithreaded and optimized various scripts

--- Data/Base.rte/Effects/Pyro/Flame/GroundFlame.lua
+++ Data/Base.rte/Effects/Pyro/Flame/GroundFlame.lua
@@ -1,3 +1,5 @@
+--[[MULTITHREAD]]--
+
 function Create(self)
 	self.shortFlame = CreatePEmitter("Flame Hurt Short Float", "Base.rte");
 	
@@ -7,6 +9,7 @@ function Create(self)
 	if self.Throttle == nil then
 		self.Throttle = 0;
 	end
+	self.flameToAdd = nil;
 end
 
 function Update(self)
@@ -18,12 +21,20 @@ function Update(self)
 	if self.flameTimer:IsPastSimMS(self.flameSpawnDelay) then
 		self.flameTimer:Reset();
 		self.flameSpawnDelay = 1000 + self.Age * 0.25;
-		
+
 		local particle = self.shortFlame:Clone();
 		particle.Lifetime = 100 + math.max(particle.Lifetime * self.ageRatio);
 		particle.Vel = self.Vel + Vector(0, -2) + Vector(math.random(), 0):RadRotate(RangeRand(-math.pi, math.pi));
 		particle.Pos = self.Pos + Vector(0, -1);
-		MovableMan:AddParticle(particle);
+		self.flameToAdd = particle;
+		self:RequestSyncedUpdate();
+	end
+end
+
+function SyncedUpdate(self)
+	if self.flameToAdd then
+		MovableMan:AddParticle(self.flameToAdd);
+		self.flameToAdd = nil;
 	end
 end
 
--- Data/Techion.rte/Devices/Weapons/NanoRifle/NanoRifleSight.lua
+++ Data/Techion.rte/Devices/Weapons/NanoRifle/NanoRifleSight.lua
@@ -9,19 +9,17 @@ function Create(self)
 	self.laserDensity = math.ceil(self.laserLength/self.laserSpaceCheck);
 
 	-- Cached value for the synced update
-	self.roughLandPos = nil;
+	self.laserPar = nil;
 end
 
 function Update(self)
-	self.roughLandPos = nil;
-
 	if not self:IsReloading() and self.laserTimer:IsPastSimMS(self.laserCheckDelay) then
 		self.laserTimer:Reset();
 		local actor = self:GetRootParent();
 		if IsActor(actor) and ToActor(actor):GetController():IsState(Controller.AIM_SHARP) then
 			local actor = ToActor(actor);
 			local aimAngle = actor:GetAimAngle(true);
-			self.roughLandPos = self.MuzzlePos + Vector(self.laserLength, 0):RadRotate(aimAngle);
+			local roughLandPos = self.MuzzlePos + Vector(self.laserLength, 0):RadRotate(aimAngle);
 			for i = 0, self.laserDensity do
 				local checkPos = self.MuzzlePos + Vector(self.laserSpaceCheck * i, 0):RadRotate(aimAngle);
 				if SceneMan.SceneWrapsX == true then
@@ -37,17 +35,17 @@ function Update(self)
 					if moCheck ~= rte.NoMOID and moCheck ~= self.ID then
 						local mo = ToMOSRotating(MovableMan:GetMOFromID(moCheck));
 						if mo and (mo.Team ~= actor.Team or (mo.WoundCount > 0 and mo.PresetName ~= "Nano Rifle")) then
-							self.roughLandPos = checkPos;
+							roughLandPos = checkPos;
 							break;
 						end
 					end
 				else
-					self.roughLandPos = checkPos;
+					roughLandPos = checkPos;
 					break;
 				end
 			end
 
-			local checkRoughLandPos = self.roughLandPos + Vector(self.laserSpaceCheck * -1, 0):RadRotate(aimAngle);
+			local checkRoughLandPos = roughLandPos + Vector(self.laserSpaceCheck * -1, 0):RadRotate(aimAngle);
 			for i = 0, self.laserSpaceCheck do
 				local checkPos = checkRoughLandPos + Vector(i, 0):RadRotate(aimAngle);
 				if SceneMan.SceneWrapsX == true then
@@ -70,17 +68,19 @@ function Update(self)
 				else
 					break;
 				end
-				self.roughLandPos = checkPos;
+				local laserPar = CreateMOPixel("Nano Rifle Laser Sight Glow");
+				laserPar.Pos = checkPos;
+				laserPar.Lifetime = self.laserCheckDelay * 2;
+				self.laserPar = laserPar;
+				self:RequestSyncedUpdate();
 			end
 		end
 	end
 end
 
 function SyncedUpdate(self)
-	if self.roughLandPos then
-		local laserPar = CreateMOPixel("Nano Rifle Laser Sight Glow");
-		laserPar.Pos = self.roughLandPos;
-		laserPar.Lifetime = self.laserCheckDelay * 2;
-		MovableMan:AddParticle(laserPar);
+	if self.laserPar then
+		MovableMan:AddParticle(self.laserPar);
+		self.laserPar = nil;
 	end
 end
